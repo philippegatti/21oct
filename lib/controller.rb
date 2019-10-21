58ca7074ef3955f14a0e6a9960f83dca8de56759
philippegatti <ph.gatti@gmail.com>
2019-10-21 20:10:14 +0200

    with comments

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,30 +2,30 @@ require 'gossip'
 
 class ApplicationController < Sinatra::Base
   
-  get '/' do
+  get '/' do #affiche la page principale
 	erb :index, locals: {gossips: Gossip.all}
   end
   
-  get '/gossips/new/' do
+  get '/gossips/new/' do #affiche la page de création d'un nouveau gossip
     erb :new_gossip
   end
   
-  get '/gossips/:id' do
+  get '/gossips/:id' do #affiche la page d'un gossip donné
   	erb :show, locals: {gossip: Gossip.find(params["id"].to_i), id: params["id"]}
   end
 
-  post '/gossips/new/' do
+  post '/gossips/new/' do #récupère les informations et crée un nouveau gossip
   	Gossip.new(params["gossip_author"],params["gossip_content"]).save
   	redirect '/'
   end
 
-  get '/gossips/:id/edit/' do
+  get '/gossips/:id/edit/' do #affiche la page permettant de modifier un gossip
   	erb :edit, locals: {gossip: Gossip.find(params["id"].to_i), id: params["id"]}
   end
 
-  post '/gossips/:id/edit/' do
-  	gossip_updated = Gossip.all[params["id"].to_i-1].update(params["gossip_author"],params["gossip_content"])
-  	Gossip.save_update(gossip_updated,params["id"].to_i)
+  post '/gossips/:id/edit/' do #recupère les infos et modifie le gossip
+  	gossip_updated = Gossip.all[params["id"].to_i-1].update(params["gossip_author"],params["gossip_content"]) # on modifie les variables de l'instance gossip
+  	Gossip.save_update(gossip_updated,params["id"].to_i) #on enregistre les changements dans la bdd
   	redirect '/'
   end
 
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -10,13 +10,13 @@ def initialize (author, content)
 	@content = content
 end
 
-def save
+def save #sauvegarde d'un gossip dans le csv
   CSV.open("./db/gossip.csv", "ab") do |csv|
     csv << [@author,@content]
   end
 end
 
-def self.all
+def self.all #recensement de tous les gossips
   all_gossips = []
   CSV.read("./db/gossip.csv").each do |csv_line|
     all_gossips << Gossip.new(csv_line[0], csv_line[1])
@@ -34,16 +34,10 @@ def update(author, content)
 	return self
 end
 
-def self.save_update(updated_gossip,i)
+def self.save_update(updated_gossip,i) #enregistrement du gossip modifié
 		gossip_csv = []
 		gossip_csv = CSV.table("./db/gossip.csv")
-		puts "gossips avant changements"
-		puts gossip_csv
-		puts "gossip à modifier"
-		puts gossip_csv[i-2] 
 		gossip_csv[i-2] = [updated_gossip.author,updated_gossip.content]
-		puts"gossips modifiés avant sauvegarde"
-		puts gossip_csv
 		File.open("./db/gossip.csv", 'w') do |f|
   			f.write(gossip_csv.to_csv)
 		end
